0209c61e5c2d3679f7b72426a624b49ecbca0d8d
JonathnSparePartsFuller <jonefuller@gmail.com>
2022-05-17 14:56:38 -0700

    Added more info about the app and fixed the test file

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 export default class Home extends Component {
   render() {
     return (
-      <h3>Home</h3>
+      <>
+        <h3>View our Wonderful Apartments</h3>
+        <p>A fine selection of Apartments</p>
+      </>
     )
   }
 }
